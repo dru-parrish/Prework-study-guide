68cea833a02a02c9e277b1efce7aa0878f448786
[dru parrish] <[dru.parrish@gmail.com]>
2023-11-21 18:22:10 -0600

    added JavaScript and linked to HTML

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+/*
+if (topic === 'HTML') {
+    console.log("Let us study HTML!");
+ } else if (topic === 'CSS') {
+    console.log("Let us study CSS!");
+ }
+else if (topic === 'Git') {
+    console.logic("Let us study Git!");
+ }
+else if (topic === 'JavaScript') {
+    console.log("Let us study JavaScript");
+} else {
+     console.log('Please try again!');
+    }
+  */
+
+    for (var x = 0; x <topics.length; x++) {
+        console.log(topics[x]);
+    }
